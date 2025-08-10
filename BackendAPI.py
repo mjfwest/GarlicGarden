436963ba64fe5f5867a143385cbd996727f4bd6e
Janjre <theshepherds555@gmail.com>
2025-08-10 12:18:19 +0100

    Added a graph and webpage!!

--- BackendAPI.py
+++ BackendAPI.py
@@ -1,7 +1,11 @@
-﻿from flask import Flask, request, jsonify
+﻿import flask
+from flask import Flask, request, jsonify
 
 #make app
-app = Flask(__name__)
+app = Flask(__name__,
+            static_url_path='',
+            static_folder='web/static',
+            template_folder='web/templates')
 
 ##set startubg varuabkes
 sensor_data = {"moisture" : 0}
@@ -11,6 +15,11 @@ pump_state = {"pump": False}
 # /status GET returns sensor and pump unpacked in a list ✅
 # /pump GET return pump_state ✅
 # /pump POST  sets the pump state to the value if it is there and false if it isn't, returns status ✅
+
+# @app.route('/', methods=["GET"])
+# def serve_website():
+#     print("got here")
+#     return flask.send_from_directory('templates', 'index.html')
 
 @app.route("/sensor", methods=["POST"])
 def receive_sensor():
@@ -37,6 +46,25 @@ def button():
     data = request.json
     print(data)
     return jsonify({"status","ok"})
+
+@app.route("/data", methods=["GET"])
+def make_up_data():
+    data = [
+        {"date": 1, "value": 10},
+        {"date": 2, "value": 65},
+        {"date": 3, "value": 96},
+        {"date": 4, "value": 97},
+        {"date": 5, "value": 97},
+        {"date": 6, "value": 19},
+        {"date": 7, "value": 67},
+        {"date": 8, "value": 13},
+        {"date": 9, "value": 43},
+        {"date": 10, "value": 54},
+        {"date": 11, "value": 13},
+        {"date": 12, "value": 98}
+    ]
+
+    return jsonify(data)
 
 
 
